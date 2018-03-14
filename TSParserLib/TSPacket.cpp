d2db01b0c19c784ae21c814da430c8e7db29ebc1
yinwenjie <eternal.ywj@gmail.com>
2018-03-14 17:51:08 +0800

    Add support for Adaptation field

--- TSParserLib/TSPacket.cpp
+++ TSParserLib/TSPacket.cpp
@@ -28,10 +28,16 @@ int CTransportStreamPacket::Parse_one_ts_packet(BYTE *buffer)
 
 	trasport_scrambling_control = (buffer[4] >> 6) & 3;
 	adaption_field_control = (buffer[4] >> 4) & 3;
-	transport_priority = buffer[4] & 3;
+	continuity_counter = buffer[4] & 15;
+
+	if (adaption_field_control == 2 || adaption_field_control == 3)
+	{
+		parse_adaptation_field(buffer + 5);
+	}
 
 	return kTSParserError_NoError;
 }
+
 
 void CTransportStreamPacket::Dump_packet_info(int pkt_idx)
 {
@@ -59,5 +65,43 @@ void CTransportStreamPacket::Dump_packet_info(int pkt_idx)
 	g_logoutFile << "trasport_scrambling_control = " << to_string(trasport_scrambling_control) << endl;
 	g_logoutFile << "adaption_field_control = " << to_string(adaption_field_control) << endl;
 	g_logoutFile << "continuity_counter = " << to_string(continuity_counter) << endl;
+	if (adaption_field_control == 2 || adaption_field_control == 3)
+	{
+		dump_adaptation_field();
+	}
 #endif
+}
+
+int CTransportStreamPacket::parse_adaptation_field(BYTE *buffer)
+{
+	adaption_field.adaptation_field_length = buffer[0];
+	if (adaption_field.adaptation_field_length > 0)
+	{
+		adaption_field.discontinuity_indicator = buffer[1] & (1 << 0);
+		adaption_field.random_access_indicator = buffer[1] & (1 << 1);
+		adaption_field.elementary_stream_priority_indicator = buffer[1] & (1 << 2);
+		adaption_field.PCR_flag = buffer[1] & (1 << 3);
+		adaption_field.OPCR_flag = buffer[1] & (1 << 4);
+		adaption_field.splicing_point_flag = buffer[1] & (1 << 5);
+		adaption_field.transport_private_data_flag = buffer[1] & (1 << 6);
+		adaption_field.adaptation_field_extension_flag = buffer[1] & (1 << 7);
+	}
+
+	return kTSParserError_NoError;
+}
+
+void CTransportStreamPacket::dump_adaptation_field()
+{
+	g_logoutFile << "adaptation_field_length: " << to_string(adaption_field.adaptation_field_length) << endl;
+	if (adaption_field.adaptation_field_length > 0)
+	{
+		g_logoutFile << "discontinuity_indicator: " << to_string(adaption_field.discontinuity_indicator) << endl;
+		g_logoutFile << "random_access_indicator: " << to_string(adaption_field.random_access_indicator) << endl;
+		g_logoutFile << "elementary_stream_priority_indicator: " << to_string(adaption_field.elementary_stream_priority_indicator) << endl;
+		g_logoutFile << "PCR_flag: " << to_string(adaption_field.PCR_flag) << endl;
+		g_logoutFile << "OPCR_flag: " << to_string(adaption_field.OPCR_flag) << endl;
+		g_logoutFile << "splicing_point_flag: " << to_string(adaption_field.splicing_point_flag) << endl;
+		g_logoutFile << "transport_private_data_flag: " << to_string(adaption_field.transport_private_data_flag) << endl;
+		g_logoutFile << "adaptation_field_extension_flag: " << to_string(adaption_field.adaptation_field_extension_flag) << endl;
+	}
 }
--- TSParserLib/TSPacket.h
+++ TSParserLib/TSPacket.h
@@ -1,6 +1,19 @@
 #ifndef _TS_PACKET_H_
 #define _TS_PACKET_H_
 
+typedef struct Adaptation_field
+{
+	UINT8 adaptation_field_length;
+	bool discontinuity_indicator;
+	bool random_access_indicator;
+	bool elementary_stream_priority_indicator;
+	bool PCR_flag;
+	bool OPCR_flag;
+	bool splicing_point_flag;
+	bool transport_private_data_flag;
+	bool adaptation_field_extension_flag;
+} Adaptation_field;
+
 class CTransportStreamPacket
 {
 public:
@@ -19,9 +32,14 @@ public:
 	UINT8	adaption_field_control;
 	UINT8	continuity_counter;
 
+	Adaptation_field adaption_field;
+
 private:
 	BYTE*	payload_start;
 	UINT8	payload_length;
+
+	int parse_adaptation_field(BYTE *buffer);
+	void dump_adaptation_field();
 };
 
 #endif
